ac2ef525d1c052934441070d04c0a2e37050e63e
Vox Humana <jonprine@gmail.com>
2020-08-11 18:44:24 -0500

    added front end functionality

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -107,11 +109,21 @@ app.post('/api/animals', (req, res) => {
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
- // add animal to json file and animals array in this function
     const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
   }
+});
 
-  res.json(animal);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
